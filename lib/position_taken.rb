35669debfeecadd656aecfab8870a9548bf8b525
mgiorgis <graciosa@gmail.com>
2016-03-05 22:31:52 +0000

    nitrous-backup at Sat Mar  5 22:31:52 UTC 2016

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, position)
-#   position = position.to_i - 1
   if board[position] == " " || board[position] == "" || board[position] == nil
     false
   elsif board[position] == "X" || board[position] == "O"
     true
-  end 
+  end
 end
